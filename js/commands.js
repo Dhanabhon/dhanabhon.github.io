a98b75e5055af32c934129bd43eb0885f52dbb4d
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2022-10-05 18:18:13 +0700

    New interactive web terminal.

---
+++ js/commands.js
@@ -0,0 +1,71 @@
+var twitter = "https://twitter.com/TomDhanabhon";
+var password = "dhanabhon";
+var linkedin = "https://www.linkedin.com/in/dhanabhon/";
+var github = "https://github.com/dhanabhon/";
+var stackoverflow = "https://stackoverflow.com/users/1969570/tom-dhanabhon";
+var email = 'mailto:tom@dhanabhon.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Tom! 👋",
+  "I’m the CTO and co-founder of BrainiFit company in Bangkok, Thailand.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/tomdhanabhon' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/dhanabhon' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/dhanabhon' + "</a>",
+  'stackoverflow  <a href="' + stackoverflow + '" target="_blank">stackoverflow/dhanabhon' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">credit</span>         View credit',
+  "<br>",
+];
+
+banner = [
+    '<span class="index">DHANABHON.COM Not A Corporation. All rights reserved.</span>',
+    " ____  _   _    _    _   _    _    ____  _   _  ___  _   _   ____ ___  __  __ ",
+    "|  _ \\| | | |  / \\  | \\ | |  / \\  | __ )| | | |/ _ \\| \\ | | / ___/ _ \\|  \\/  |",
+    "| | | | |_| | / _ \\ |  \\| | / _ \\ |  _ \\| |_| | | | |  \\| || |  | | | | |\\/| |",
+    "| |_| |  _  |/ ___ \\| |\\  |/ ___ \\| |_) |  _  | |_| | |\\  || |__| |_| | |  | |",
+    "|____/|_| |_/_/   \\_\\_| \\_/_/   \\_\\____/|_| |_|\\___/|_| \\_(_)____\\___/|_|  |_|",
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",                                                                          
+];
+
+credit = [
+    "This website uses the source code from fkcodes.com.",
+];
