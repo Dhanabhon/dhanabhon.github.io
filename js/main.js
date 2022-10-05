cf6913b8d2da8af335bba94a8c24e4af3bf9132e
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2022-10-05 18:36:26 +0700

    Change visitor to anonymous.

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ function enterKey(e) {
     if (e.keyCode == 13) {
       commands.push(command.innerHTML);
       git = commands.length;
-      addLine("visitor@dhanabhon.com:~$ " + command.innerHTML, "no-animation", 0);
+      addLine("anonymous@dhanabhon.com:~$ " + command.innerHTML, "no-animation", 0);
       commander(command.innerHTML.toLowerCase());
       command.innerHTML = "";
       textarea.value = "";
