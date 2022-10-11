031fbd982eebf21c310008e48964e66983d5c13d
Dhanabhon Subha-asavabhokhin <thanaphon.spk@gmail.com>
2022-10-11 15:23:29 +0700

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -62,7 +62,7 @@ banner = [
     "| | | | |_| | / _ \\ |  \\| | / _ \\ |  _ \\| |_| | | | |  \\| || |  | | | | |\\/| |",
     "| |_| |  _  |/ ___ \\| |\\  |/ ___ \\| |_) |  _  | |_| | |\\  || |__| |_| | |  | |",
     "|____/|_| |_/_/   \\_\\_| \\_/_/   \\_\\____/|_| |_|\\___/|_| \\_(_)____\\___/|_|  |_|",
-    "                                                                                © 2022",
+    "                                                                       © 2022",
     '<span class="color2">Welcome to my interactive web terminal.</span>',
     "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",                                                                          
 ];
